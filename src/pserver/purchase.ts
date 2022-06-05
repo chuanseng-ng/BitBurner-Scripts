d47ba8581fcb9793eac419f0c2c0007db904f417
Ng Chuan Seng <waelectriz@gmail.com>
2022-06-05 10:00:18 +0800

    Fix hostname issue and move sleep in purchase

--- src/pserver/purchase.ts
+++ src/pserver/purchase.ts
@@ -5,8 +5,9 @@ export async function purchaseServer(ns, serverCount, scannedServersFiltered) {
     var defaultRamSize = 8;
 
     while (serverCount < ns.getPurchasedServerLimit()) {
-        if (ns.getServerMaxMoney("home") > ns.getPurchasedServerCost(defaultRamSize)) {
-            var hostName = ns.purchaseServer("pserv-" + serverCount, defaultRamSize);
+        if (ns.getServerMaxMoney("home") > ns.getPurchasedServerCost(defaultRamSize) || ns.getServerMaxMoney("home") == 0) {
+            let hostName = "pserv-" + serverCount
+            ns.purchaseServer(hostName, defaultRamSize);
             let hostNameServer = {hostname: hostName, hacklevel: ns.getServerRequiredHackingLevel(hostName), maxmoney: ns.getServerMaxMoney(hostName),
                                     growth: ns.getServerGrowth(hostName), minsecurity: ns.getServerMinSecurityLevel(hostName),
                                     ramsize: ns.getServerMaxRam(hostName), numports: ns.getServerNumPortsRequired(hostName)}
@@ -15,9 +16,9 @@ export async function purchaseServer(ns, serverCount, scannedServersFiltered) {
             serverCount += 1
             //await serverCal(ns, scannedServersFiltered)
             ns.run("/build/util/serverCal.js", 1)
-        } else {
-            await ns.sleep(30000)
         }
+
+        await ns.sleep(3000)
     }
 
     return [serverCount, scannedServersFiltered]
