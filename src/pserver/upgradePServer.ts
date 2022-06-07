9862178b40e5d2d8721b52713f2cc6c5f65e9558
Ng Chuan Seng <waelectriz@gmail.com>
2022-06-07 17:39:51 +0800

    Add variable to check if enough to upgrade

--- src/pserver/upgradePServer.ts
+++ src/pserver/upgradePServer.ts
@@ -2,7 +2,8 @@
 
 /** @param {NS ns} **/
 //export async function upgradeServer(ns, scannedServersFiltered) {
-export async function main(ns) {
+export function main(ns) {
+    let poorChecker = 0
     let startRamSize = 8
     let pserverMaxRam = ns.getPurchasedServerMaxRam()
     let playerMoney = ns.getPlayer().money
@@ -24,17 +25,19 @@ export async function main(ns) {
         ns.tprint("Can't afford upgrade - current " + currentServerSize + "GB, can afford " + startRamSize + "GB");
         ns.tprint("Next server RAM upgrade is " + currentServerSize * 2 + "GB which costs " +
                     ns.getPurchasedServerCost(startRamSize * 2) / 1000 / 1000 + "bil")
-        await ns.sleep(60000);
+        poorChecker = 1
     }
 
-    ns.tprint("Buying " + existingServers.length + " " + startRamSize + "GB servers")
-    for (let i = 0; i < existingServers.length; i++) {
-        if (ns.serverExists(existingServers[i])) {
-            ns.killall(existingServers[i]);
-            ns.deleteServer(existingServers[i]);
-        }
-        ns.purchaseServer(existingServers[i], startRamSize);
+    if (poorChecker != 1) {
+        ns.tprint("Buying " + existingServers.length + " " + startRamSize + "GB servers")
+        for (let i = 0; i < existingServers.length; i++) {
+            if (ns.serverExists(existingServers[i])) {
+                ns.killall(existingServers[i]);
+                ns.deleteServer(existingServers[i]);
+            }
+            ns.purchaseServer(existingServers[i], startRamSize);
 
-        //await serverCal(ns, scannedServersFiltered)
+            //await serverCal(ns, scannedServersFiltered)
+        }
     }
 }
