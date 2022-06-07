732965b0bde375070c5973230920b2c800b97286
Ng Chuan Seng <waelectriz@gmail.com>
2022-06-07 17:29:01 +0800

    Fix script running with 0 threads

--- src/util/serverCal.ts
+++ src/util/serverCal.ts
@@ -58,5 +58,8 @@ async function serverExec(ns, scannedServersFiltered, highestLvlServer, optimalS
         ns.exec("/build/exec/hack.js", scannedServersFiltered[i].hostname, Math.floor(scannedServersFiltered[i].ramsize/2.4), highestLvlServer[optimalServerIndex].hostname)
     }
 
-    ns.run("/build/exec/hack.js", Math.floor(homeram - 32)/2.4, highestLvlServer[optimalServerIndex].hostname)    
+    let homeThread = Math.floor(homeram - 32)/2.4
+    if (homeThread > 0) {
+        ns.run("/build/exec/hack.js", Math.floor(homeram - 32)/2.4, highestLvlServer[optimalServerIndex].hostname)    
+    }
 }
