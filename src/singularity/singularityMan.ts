52a7a18190545a87a485dd52131688373a65b50d
Chuanseng.Ng <waelectriz@gmail.com>
2022-10-08 15:25:03 +0800

    Update for augment install and installed count

--- src/singularity/singularityMan.ts
+++ src/singularity/singularityMan.ts
@@ -1,17 +1,21 @@
 /** @param {NS ns} **/
 export async function main(ns) {
-    let programHackLvl = {brutessh: 50, ftpcrack: 100, relaysmtp: 250, httpworm: 500, sqlinject: 750, 
-                            deepscanv1: 75, deepscanv2: 400, serverprofile: 75, autolink: 25};
-    let factionList    = ns.getPlayer().factions;
-    let busyCheck      = false;
-    let focusCheck     = false;
-    let poorCheck      = false;
+    let programHackLvl      = {brutessh: 50, ftpcrack: 100, relaysmtp: 250, httpworm: 500, sqlinject: 750, 
+                                deepscanv1: 75, deepscanv2: 400, serverprofile: 75, autolink: 25};
+    let factionList         = ns.getPlayer().factions;
+    let bitnodePriorityDict = {1.0: 3, 5.0: 1, 2.0: 3, 3.0: 3, 4.0: 3, 5.1: 3, 6.0: 1, 7.0: 1, 6.1: 3, 
+                                7.1: 3, 9.0: 1, 10.0: 1, 9.1: 3, 10.1: 3, 8.0: 3, 11.0: 3, 13.0: 3, 12.0: 10};
+    let installAugmentCount = 0;
+    let busyCheck           = false;
+    let focusCheck          = false;
+    //let poorCheck          = false;
 
     // Always start check if isBusy and isFocused
     if (!ns.singularity.isBusy && !ns.singularity.isFocused) {
         while (!busyCheck && !focusCheck) {
             // Check if able to create hack program
             if (ns.getPlayer().skill.intelligence < 255) {
+                ns.tprint("Int level is under 255, will create programs");
                 for (let programHack in programHackLvl) {
                     if(ns.getPlayer().skill.hacking > programHackLvl[programHack]) {
                         ns.singularity.createProgram(programHack);
@@ -40,14 +44,20 @@ export async function main(ns) {
                     if (ns.getPlayer().money >= ns.singularity.getAugmentationPrice(factionAugment) && 
                         currFactionRep >= ns.singularity.getAugmentationRepCost(factionAugment) && 
                         !ownedAugmentList.includes(factionAugment)) {
+                        ns.tprint("Purchasing " + factionAugment + " from " + factionList[i]);
                         ns.singularity.purchaseAugmentation(factionAugment);
-                    
+                        installAugmentCount++;
                     }
                 }
             }
-            // Install augments when there is export game bonus
+            // Install augments when there is export game bonus or when there are more than 10 augments queued
+            if (ns.singularity.exportGameBonus() || installAugmentCount > 9) {
+                ns.tprint("Installing purchased augments and rerunning main script after reset");
+                ns.singularity.installAugmentations("/build/main.js");
+            }
 
             // Upgrade home server core number/RAM if able to
+            //// If update home server RAM, kill hack script in home and rerun serverCalc
 
             // Auto commit crime if not busy by this point
         }
